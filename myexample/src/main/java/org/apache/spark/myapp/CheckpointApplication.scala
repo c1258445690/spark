c2d9a5138e151f22e83c8c94810ef275515316d4
chenwenjie <1258445690@qq.com>
2023-11-27 21:45:51 +0800

    1

--- myexample/src/main/java/org/apache/spark/myapp/CheckpointApplication.scala
+++ myexample/src/main/java/org/apache/spark/myapp/CheckpointApplication.scala
@@ -3,12 +3,19 @@ package org.apache.spark.myapp
 
 import org.apache.spark.sql.SparkSession
 
+import java.time.LocalDateTime
+
 object CheckpointApplication extends App {
+//  日志可以通过yarn查看：yarn logs  -applicationId application_1699838710415_0009
+  val logger = org.slf4j.LoggerFactory.getLogger(this.getClass)
+  logger.info("CheckpointApplication start:"+ LocalDateTime.now())
+
   val sc = SparkSession
     .builder()
     .appName("CheckpointApplication")
     .getOrCreate()
     .sparkContext
+  sc.setCheckpointDir("hdfs://192.168.1.66:9001/home/cwj/mycheckpoint")
   val inputRDD = sc.parallelize(Array[(Int, String)](
     (1, "a"), (2, "b"), (3, "c"), (4, "d"), (5, "e"), (3, "f"), (2, "g"), (1, "h")
   ), 3)
@@ -16,7 +23,6 @@ object CheckpointApplication extends App {
   mappedRDD.cache()
   val reducedRDD = mappedRDD.reduceByKey((x, y) => x + "_" + y, 2)
   reducedRDD.cache()
-//  reducedRDD.ch
   reducedRDD.checkpoint()
   reducedRDD.foreach(println)
   val groupedRDD = mappedRDD.groupByKey().mapValues(v => v.toList)
@@ -25,6 +31,8 @@ object CheckpointApplication extends App {
   groupedRDD.foreach(println)
   val joinedRDD = reducedRDD.join(groupedRDD)
   joinedRDD.foreach(println)
+  logger.info("CheckpointApplication end:"+ LocalDateTime.now())
+
 }
 
 
