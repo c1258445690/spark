333d427fcfd4e057f579b268cea4e08d8e05f32f
chen_wenjie <1258445690@qq.com>
2023-12-18 11:58:42 +0800

    myexample

--- myexample/src/main/java/org/apache/spark/myapp/sparksql/SimpleSqlQuery.scala
+++ myexample/src/main/java/org/apache/spark/myapp/sparksql/SimpleSqlQuery.scala
@@ -2,12 +2,18 @@
 package org.apache.spark.myapp.sparksql
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.internal.SQLConf.PLAN_CHANGE_LOG_LEVEL
 
 object SimpleSqlQuery extends App {
   val spark = SparkSession.builder()
     .appName("SimpleSqlQuery")
     .master("local")
     .getOrCreate()
+  val sqlConf = new SQLConf
+  //打印RuleExecutor执行的Rule,Applying Rule
+  sqlConf.setConf(PLAN_CHANGE_LOG_LEVEL, "info")
+  SQLConf.setSQLConfGetter(() =>sqlConf)
 //  org/apache/spark/sql/catalyst/parser/SqlBaseParser.g4
   spark.read
     .json("spark/student.json")
